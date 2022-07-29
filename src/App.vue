50dc4bd905c40c0bab836adee2bdebc9502f17f5
parkchino <cksdnfjqm119@gmail.com>
2022-07-29 16:42:25 +0900

    뷰 페이징처리

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <h1>TABLE PAGINATION</h1>
+    <paginated-list :list-array="pageArray" />
+  </div>
 </template>
 
+<script>
+import axios from 'axios'
+import PaginatedList from './PaginatedList'
+export default {
+  name: 'simple-pagination',
+  components: {
+    PaginatedList
+  },
+  data() {
+    return {
+      pageArray: []
+    }
+  },
+  created() {
+    axios
+      .get('http://sample.bmaster.kro.kr/contacts')
+      .then((response) => {
+        console.log(response)
+        this.pageArray = response.data.contacts
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+h1 {
+  color: #454545;
   text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
